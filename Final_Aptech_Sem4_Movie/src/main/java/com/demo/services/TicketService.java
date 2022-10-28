1e46a6eede5851b57c9afef304236c6a88144d8e
annguyenba69@gmail.com <annguyenba69@gmail.com>
2022-10-28 20:29:12 +0700

    fixUpdateUserAn

--- Final_Aptech_Sem4_Movie/src/main/java/com/demo/services/TicketService.java
+++ Final_Aptech_Sem4_Movie/src/main/java/com/demo/services/TicketService.java
@@ -15,10 +15,8 @@ import com.demo.models.Ticket;
 
 public interface TicketService {
 
-<<<<<<< HEAD
 
-=======
+
 	public List<Ticket> findAllByUserId(int user_id);
-	
->>>>>>> e14c949 (user template)
+
 }
